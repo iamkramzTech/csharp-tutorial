7f537cbab4741be28f5b778f6a5b573b2db239cb
iamkramzTech <bernaldezm88@yahoo.com>
2025-10-23 13:45:39 +0800

    Added Project files

---
+++ Day01ConsoleVariableAndDataTypes/Day01ConsoleVariableAndDataTypes/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace Day01ConsoleVariableAndDataTypes
+{
+    internal class Program
+    {
+        //Your code start executing here in Main method
+        static void Main(string[] args)
+        {
+            Run();
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Get string user input 
+        /// </summary>
+        /// <param name="prompt">Message to be Display in Console</param>
+        /// <returns>user input in string format</returns>
+        static string? GetStringPrompt(string prompt)
+        {
+            Console.Write(prompt);
+            return Console.ReadLine();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="prompt">string prompt for age</param>
+        /// <returns>age</returns>
+        static int GetAgePrompt(string prompt)
+        {
+            Console.Write(prompt);
+            while (true)
+            {
+                bool success = int.TryParse(Console.ReadLine(), out var age);
+                if(success && !(age < 0) )
+                {
+                    return age;
+                }
+                Console.Write("Invalid age. Please enter a valid age number: ");
+
+            }
+        }
+        /// <summary>
+        /// inline expression getting birth year by subtracting age from the current year
+        /// </summary>
+        /// <param name="age">int age</param>
+        /// <returns>birth year /returns>
+       static int GetBirthYear(int age) => DateTime.UtcNow.Year - age;
+        /// <summary>
+        /// Display Hello Message
+        /// </summary>
+        /// <param name="name">string name</param>
+        /// <param name="age">int age</param>
+        /// <param name="favoriteProgrammingLanguage">string favorite</param>
+       /// <param name="birthYear">int birthYear</param>
+        static void DisplayHelloMessage(string? name, int age, string? favoriteProgrammingLanguage, int birthYear)
+        {
+
+            Console.WriteLine(); //Ensure it has a new line spacing
+            Console.WriteLine($"Hello {name}! You are {age} years old and you love {favoriteProgrammingLanguage}.");
+            Console.WriteLine($"You were born in {birthYear}.");
+        }
+     /// <summary>
+     /// Run a Driver's method
+     /// </summary>
+        static void Run()
+        {
+            string? firstName = GetStringPrompt("Enter your name: ");
+            int age = GetAgePrompt("Enter your age: ");
+            string? favoriteProgrammingLanguage = GetStringPrompt("Enter your favorite programming language: ");
+            int birthYear = GetBirthYear(age);
+            DisplayHelloMessage(firstName, age, favoriteProgrammingLanguage,birthYear);
+        } 
+    }
+}
---
+++ Day01ConsoleVariableAndDataTypes/Day01ConsoleVariableAndDataTypes/VariableAndDataTypes.cs
@@ -0,0 +1,36 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+
+namespace Day01ConsoleVariableAndDataTypes
+{
+    internal class VariableAndDataTypes
+    {
+        //In C#, there are different types of variables (defined with different keywords), for example:
+        //int - stores integers(whole numbers), without decimals, such as 123 or -123
+        //double - stores floating point numbers, with decimals, such as 19.99 or -19.99
+        //char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+        //string - stores text, such as "Hello World". String values are surrounded by double quotes
+        //bool - stores values with two states: true or false
+
+        //Format in Declaring (Creating Variables)
+        //  <type> variableName = value;
+        //Example:
+        string name = "John";
+        string greetings = "Hello";
+        int age = 26;
+        bool isHandsome = false; // data type for true or false 
+
+      *Required:  Please Read and follow the online References because it can be help you in learning C# Programming language
+
+        //Online References:
+        //W3School Website
+        // Variables : https://www.w3schools.com/cs/cs_variables.php
+        // Data Types       : https://www.w3schools.com/cs/cs_data_types.php
+
+        // Learn More
+        //1. What is Csharp :  https://www.w3schools.com/cs/cs_intro.php
+        //2. Get Started    : https://www.w3schools.com/cs/cs_getstarted.php
+        //3. C#  Syntax     : https://www.w3schools.com/cs/cs_syntax.php
+    }
+}
